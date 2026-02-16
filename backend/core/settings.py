9f42278088be1605913befc57cfb2c63e26c9e7c
Utujevan <utujevan@gmail.com>
2026-02-16 18:53:02 +0200

    Implement API documentation

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
     "ml",
     "api",
     "db",
@@ -136,3 +137,14 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Appointment Predictor API',
+    'DESCRIPTION': 'API for Medical Appointment No-Show Prediction',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
